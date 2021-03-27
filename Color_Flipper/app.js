32a2812365e9f4f908bd90c8ea4cc60f50ab5025
edolly2 <edolly2@protonmail.com>
2021-03-26 21:16:31 -0500

    "app.js"

--- Color_Flipper/app.js
+++ Color_Flipper/app.js
@@ -1,1 +1,16 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+// GET RANDOM NUMBER FUNCTION
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
+
+
+btn.addEventListener('click', () => {
+    // GET RANDOM NUMBER BETWEEN 1-3 FOR ARRAY INDEX
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+})
